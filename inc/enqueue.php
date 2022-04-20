9b5d8654d3c5cbddc8a29c752907aac134753e9c
woodwardtw <twwoodward@me.com>
2022-04-20 14:34:15 -0400

    resolving mobile menu issues

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -33,6 +33,7 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 		wp_enqueue_script( 'jquery' );
 
 		$js_version = $theme_version . '.' . filemtime( get_template_directory() . $theme_scripts );
+		wp_enqueue_script( 'observatory-scripts', get_template_directory_uri() . '/js/observatory.js', array(), $js_version, true );
 		wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . $theme_scripts, array(), $js_version, true );
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
