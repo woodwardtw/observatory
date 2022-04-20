0d909fe982f67a0ac1b13a5c9e8516df84b45752
woodwardtw <twwoodward@me.com>
2022-04-20 14:44:33 -0400

    mobile menu

--- js/observatory.js
+++ js/observatory.js
@@ -1,1 +1,8 @@
-console.log('foo')
+let menuButton = document.querySelector('.navbar-toggler');
+menuButton.addEventListener('click', menuFixer);
+
+function menuFixer(){
+    let mainMenu = document.getElementById('main-nav');
+    console.log(mainMenu);
+    mainMenu.classList.toggle('sharp')
+}
