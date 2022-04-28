fb2eb5be69fae769e25decf8d0dd64ff4ce5fd9f
woodwardtw <twwoodward@me.com>
2022-04-28 08:08:33 -0400

    tweaks

--- single-external.php
+++ single-external.php
@@ -12,7 +12,7 @@ get_header();
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
-<div class="wrapper" id="single-wrapper">
+<div class="wrapper external" id="single-wrapper">
 
 	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
 
